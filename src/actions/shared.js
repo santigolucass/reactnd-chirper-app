9ecc2dad1b1125cc6ef19bbb015f0b395ec00ae3
Lucas <afroex.lucas@gmail.com>
2021-03-18 16:57:22 +0000

    LoadingBar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveTweets } from '../actions/tweets'
 import { receiveUsers } from '../actions/users'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading } from 'react-redux-loading'
+import { hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'sarah_edo'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading())
     })
   }
 }
