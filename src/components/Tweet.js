c3142c1285c30ad7330239f8441221203300eced
Lucas <afroex.lucas@gmail.com>
2021-03-18 16:48:40 +0000

    Tweet ui

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatTweet } from '../utils/helpers'
+import { formatTweet, formatDate } from '../utils/helpers'
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 
 class Tweet extends Component {
+
+  toParent = (e, parentId) => {
+    e.preventDefault();
+  }
+
+  handleLike = (e) => {
+    e.preventDefault();
+  }
+
   render(){
     const { tweet } = this.props
 
@@ -10,8 +20,42 @@ class Tweet extends Component {
       return <p> This tweet doesn't exist</p>
     }
 
+    const {
+      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+    } = tweet
+
     return(
-      <div className='tweet'>TWEET ID: <b>{this.props.id}</b></div>
+      <div className='tweet'>
+        <img
+          src={avatar}
+          alt={`Avatar of ${name}`}
+          className='avatar'
+        />
+        <div className='tweet-info'>
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className='replying-to' onClick={(e) => {this.toParent(e, parent.id)}}>
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className='tweet-icons'>
+            <TiArrowBackOutline className='tweet-icon'/>
+            <span>{replies !== 0 && replies}</span>
+            <button
+              className='heart-button'
+              onClick={this.handleLike}>
+              { hasLiked === true
+                ? <TiHeartFullOutline color='#e0245e' className='tweet-icon'/>
+                : <TiHeartOutline className='tweet-icon'/>}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
